7f95140f6561675de1259d2ba6dbe9d276e14b7f
Tarun Sharma <taruns@ghrix.com>
2020-08-06 04:02:15 +0000

    revert url

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,11 +17,11 @@ module.exports = {
          * The base URL of the WordPress site without the trailingslash and the protocol. This is required.
          * Example : 'demo.wp-api.org' or 'www.example-site.com'
          */
-        //~ baseUrl: `live-gatbsyjswp.pantheonsite.io`,
-        baseUrl: `159.65.145.117:8080`,
+        baseUrl: `live-gatbsyjswp.pantheonsite.io`,
+        //~ baseUrl: `159.65.145.117:8080`,
         // The protocol. This can be http or https.
-        //~ protocol: `https`,
-        protocol: `http`,
+        protocol: `https`,
+        //~ protocol: `http`,
         // Indicates whether the site is hosted on wordpress.com.
         // If false, then the assumption is made that the site is self hosted.
         // If true, then the plugin will source its content on wordpress.com using the JSON REST API V2.
